a0185cf48217fdc9321bf9a2dca7afb17926cd64
Motin <m.motin@qsoft.ru>
2024-02-04 17:22:48 +0300

    Added auth by secret middleware

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ApiSecretAuthMiddleware;
 use App\Http\Controllers\Api\VkController;
 
 Route::name('api.')->group(function () {
-    Route::prefix('/vk')->group(function () {
+    Route::prefix('/vk')->middleware(ApiSecretAuthMiddleware::class)->group(function () {
         Route::post('/callback', [VkController::class, 'callback'])->name('vk.callback');
     });
 });
