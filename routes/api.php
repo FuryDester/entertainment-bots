c4b2b5cb861f08776112a00a840b2831bdc2b5d3
Motin <m.motin@qsoft.ru>
2024-02-04 17:25:26 +0300

    Removed unused routes, added api healthcheck

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\ApiSecretAuthMiddleware;
 use App\Http\Controllers\Api\VkController;
 
 Route::name('api.')->group(function () {
+    Route::get('/healthcheck', static function (Request $request) {
+        return response()->json([
+            'healthcheck' => true,
+            'ip' => $request->ip(),
+            'host' => $request->host(),
+            'scheme' => $request->getScheme(),
+            'body' => $request->getContent(),
+        ]);
+    })->name('healthcheck');
+
     Route::prefix('/vk')->middleware(ApiSecretAuthMiddleware::class)->group(function () {
         Route::post('/callback', [VkController::class, 'callback'])->name('vk.callback');
     });
