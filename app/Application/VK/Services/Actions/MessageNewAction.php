0fe09c4da49acf1dcdeff8915f5b83c4a5b2adf4
Motin <m.motin@qsoft.ru>
2024-02-07 21:08:39 +0300

    Added test scenario to new message action, changed env telegram bot name to telegram channel

--- app/Application/VK/Services/Actions/MessageNewAction.php
+++ app/Application/VK/Services/Actions/MessageNewAction.php
@@ -4,9 +4,11 @@ namespace App\Application\VK\Services\Actions;
 
 use App\Domain\VK\Factories\Common\MessageContextDTOFactoryContract;
 use App\Domain\VK\Services\Actionable;
+use App\Infrastructure\VK\DataTransferObjects\AccessTokenDTO;
 use App\Infrastructure\VK\DataTransferObjects\Requests\CallbackRequestDTO;
 use App\Infrastructure\VK\Enums\ActionEnum;
 use Illuminate\Support\Facades\Log;
+use VK\Client\VKApiClient;
 
 final class MessageNewAction implements Actionable
 {
@@ -28,6 +30,20 @@ final class MessageNewAction implements Actionable
             $message->getPeerId(),
             $message->getText() ?: 'empty message text'
         ));
+
+        // TODO: Remove after testing
+        if (mt_rand(0, 5) === 0 && $message->getText()) {
+            Log::info('MessageNewAction: random success');
+            /** @var AccessTokenDTO $accessToken */
+            $accessToken = app(AccessTokenDTO::class);
+
+            $client = new VKApiClient();
+            $client->messages()->send($accessToken->getAccessToken(), [
+                'peer_id' => $message->getPeerId(),
+                'message' => 'Попка попугай повторяет: ' . $message->getText(),
+                'random_id' => mt_rand(0, 1000000),
+            ]);
+        }
 
         return true;
     }
--- config/logging.php
+++ config/logging.php
@@ -134,7 +134,7 @@ return [
             'handler' => FilterHandler::class,
             'level' => env('LOG_LEVEL', 'warning'),
             'with' => [
-                'handler' => new TelegramBotHandler(env('TELEGRAM_API_KEY', ''), env('TELEGRAM_BOT_NAME', 'nothing')),
+                'handler' => new TelegramBotHandler(env('TELEGRAM_API_KEY', ''), env('TELEGRAM_CHANNEL', 'nothing')),
             ],
         ]
     ],
