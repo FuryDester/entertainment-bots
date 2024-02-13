5c669d937ec47a7b7f31a3fcb1a60cdcce16e236
Motin <m.motin@qsoft.ru>
2024-02-13 18:38:04 +0300

    Edited example tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,12 +7,9 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    public function healthcheck_ok(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/healthcheck');
 
         $response->assertStatus(200);
     }
