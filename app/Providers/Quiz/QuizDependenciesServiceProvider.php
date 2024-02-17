8d440c750fe84a1f035c199300ac58ea617dfb12
Motin <m.motin@qsoft.ru>
2024-02-17 10:37:41 +0300

    Fixed namespace for quiz service

--- app/Providers/Quiz/QuizDependenciesServiceProvider.php
+++ app/Providers/Quiz/QuizDependenciesServiceProvider.php
@@ -2,13 +2,13 @@
 
 namespace App\Providers\Quiz;
 
+use App\Application\Quiz\Services\QuizService;
 use App\Domain\Quiz\Factories\QuizDTOFactoryContract;
 use App\Domain\Quiz\Repositories\QuizRepositoryContract;
 use App\Domain\Quiz\Services\QuizServiceContract;
 use App\Infrastructure\Quiz\Factories\QuizDTOFactory;
 use App\Infrastructure\Quiz\Repositories\QuizRepository;
 use App\Providers\AbstractDependencyServiceProvider;
-use QuizService;
 
 final class QuizDependenciesServiceProvider extends AbstractDependencyServiceProvider
 {
